d9862e78b7b14db03bbd709c58c947febe659212
Cornelius Ogala Michael <mikolo@engineer.com>
2026-01-27 15:46:49 +0100

    Made some addjustment

--- src/members/finance.jsx
+++ src/members/finance.jsx
@@ -1,70 +1,78 @@
-import Button from "../components/button";
-import Footer from "../components/footer";
+// import Button from "../components/button";
+// import Footer from "../components/footer";
 import FrontCard from "../components/FrontCard";
-import ListItem from "../components/ListItem";
-import PaymentCard from "../components/PaymentCard";
-import SalesItem from "../components/SalesItem";
-import InputText from "../components/InputText";
-import Slider from "../components/MovingElement";
+// import ListItem from "../components/ListItem";
+// import PaymentCard from "../components/PaymentCard";
+// import SalesItem from "../components/SalesItem";
+// import InputText from "../components/InputText";
+// import Slider from "../components/MovingElement";
+import { useState } from "react";
 import "../styles/App.css";
-import { FaChevronRight, FaImage } from "react-icons/fa";
-import Slider2 from "../components/Slider2";
+import { FaChevronRight, FaChevronDown, FaChevronUp } from "react-icons/fa";
+// import Slider2 from "../components/Slider2";
 function Finance() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [hide, setHide] = useState("");
+  const toggle = () => {
+    // Toggle logic here
+    setHide(isExpanded ? "" : "hide");
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <>
       <section className="main">
         <h1>Finance</h1>
         <FrontCard />
-        <div className="listContainer">
-          <ListItem
-            majorText="Finance"
-            minorText="Jan 20th, 03:20:23"
-            spanText=" "
-            icon={<FaImage className="icons" />}
-            button={<FaChevronRight className="icons chevron" size={14} />}
-            styleS="circle"
-          />
-          <ListItem
-            majorText="Finance"
-            minorText="Jan 27th, 03:20:23"
-            spanText=" "
-            icon={<FaImage className="icons" />}
-            button={<FaChevronRight className="icons chevron" size={14} />}
-            styleS="circle"
-          />
-        </div>
-        <PaymentCard />
-        <SalesItem />
-        <div className="listContainer">
-          <ListItem
-            icon={<FaImage className="icons" />}
-            button={<Button name="yes" />}
-          />
-          <ListItem
-            icon={<FaImage className="icons" />}
-            button={<FaChevronRight className="icons chevron" size={14} />}
-          />
-        </div>
-
-        <div className="before">
-          <div className="sliderContainer1">
-            <Slider />
+        <table className="financeTable">
+          <div className="tableHead">
+            <thead>Available Balance</thead>
           </div>
-          <div className="sliderContainer2">
-            <Slider2 />
+          <tbody>
+            <tr>
+              <td>Wallet</td>
+              <td>$1,234.56</td>
+            </tr>
+            <tr>
+              <td>Wallet</td>
+              <td>$1,234.56</td>
+              <td>
+                + N 0.09{" "}
+                <span>
+                  <FaChevronRight />
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table className="financeTable lower">
+          <div className="tableHead">
+            <thead>Available Balance</thead>
           </div>
-        </div>
-
-        <Button name="mikolo" />
-        <InputText
-          type="email"
-          onChange={(val) => {
-            console.log(val);
-          }}
-        />
-      </section>
-      <section className="theFoot">
-        <Footer />
+          <tbody>
+            <tr>
+              <td>Wallet</td>
+              <td>$1,234.56</td>
+            </tr>
+            <tr className={hide}>
+              <td>Wallet</td>
+              <td>$1,234.56</td>
+              <td>
+                + N 0.09{" "}
+                <span>
+                  <FaChevronRight />
+                </span>
+              </td>
+            </tr>
+          </tbody>
+          <div className="iconCont">
+            {isExpanded ? (
+              <FaChevronDown className="expandIcon" onClick={toggle} />
+            ) : (
+              <FaChevronUp className="expandIcon" onClick={toggle} />
+            )}
+          </div>
+        </table>
       </section>
     </>
   );
